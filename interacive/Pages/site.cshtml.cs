bbd96d808c8caafda5438e7b066e2c73d51a3007
Rahmaatef12 <153998563+Rahmaatef12@users.noreply.github.com>
2024-10-22 21:54:54 +0300

    Add project files.

---
+++ interacive/Pages/site.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace interacive.Pages
+{
+    public class siteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ interacive/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// Add event listener to change the navbar background on scroll
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('.navbar');
+
+    // Add a class 'scrolled' when the page is scrolled past 50px
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
